23224e23ca1a7b3fa4f8972e04596e21f8cd73f6
FilRip <filrip@gmail.com>
2023-12-18 18:12:49 +0100

    Add program to repaste le Path

--- ComputeLemmingsLevel/ComputeLemmingsLevel/Program.cs
+++ ComputeLemmingsLevel/ComputeLemmingsLevel/Program.cs
@@ -10,13 +10,33 @@ internal static class Program
 
     internal static void Main(string[] args)
     {
-        foreach (string file in Directory.GetFiles(repLevels, "*.ini"))
+        /*foreach (string file in Directory.GetFiles(repLevels, "*.ini"))
         {
             string[] lignes = File.ReadAllLines(file);
             List<string> listLignes = lignes.ToList();
             listLignes.Insert(0, "[Level]");
             File.Delete(file);
             File.WriteAllLines(file, listLignes);
+        }*/
+        foreach (string file in Directory.GetFiles(@"D:\Code\FilRip\Lemmings.NET\LemRip\Content\levels", "*.ini"))
+        {
+            string[] lignes = File.ReadAllLines(file);
+            bool changement = false;
+            for (int i = 0; i < lignes.Length; i++)
+            {
+                string ligne = lignes[i];
+                if (ligne.Contains("Path["))
+                {
+                    int j = int.Parse(ligne.Split('=')[0].Replace("Path[", "").Replace("]", "")) + 1;
+                    lignes[i] = $"Path{j}=" + ligne.Split('=')[1];
+                    changement = true;
+                }
+            }
+            if (changement)
+            {
+                File.Delete(file);
+                File.WriteAllLines(file, lignes);
+            }
         }
     }
 }
